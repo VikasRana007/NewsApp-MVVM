253c5d8ef7338eb04783711da077e202dbbc96c9
Vikas Rana <vicay0001@gmail.com>
2026-05-09 14:59:55 +0530

    offline top headlines

---
+++ app/src/main/java/me/vikas/newsapp/ui/screens/offline_topheadline/OfflineTopHeadlineScreen.kt
@@ -0,0 +1,101 @@
+package me.vikas.newsapp.ui.screens.offline_topheadline
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import me.vikas.newsapp.data.local.entity.topheadlines.ArticleEntity
+import me.vikas.newsapp.ui.base.ShowError
+import me.vikas.newsapp.ui.base.ShowLoading
+import me.vikas.newsapp.ui.base.UiState
+import me.vikas.newsapp.ui.theme.NewsAppColors
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun OfflineTopHeadlineRoutes(
+    onNewsClick: (url: String) -> Unit,
+    offlineTopHeadlineViewModel: OfflineTopHeadlineViewModel = hiltViewModel(),
+    onBackClick: () -> Unit
+) {
+
+    val uiState by offlineTopHeadlineViewModel.uiState.collectAsStateWithLifecycle()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Offline Top Headlines", style = MaterialTheme.typography.titleLarge
+                            .copy(
+                            fontWeight = FontWeight.Bold
+                        ), color = NewsAppColors.White
+                    )
+                }, navigationIcon = {
+                    IconButton(onClick = onBackClick) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back",
+                            tint = NewsAppColors.White
+                        )
+                    }
+                }, colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = NewsAppColors.PrimaryColor,
+                    titleContentColor = MaterialTheme.colorScheme.background
+                )
+            )
+        }) { paddingValues ->
+        Column(modifier = Modifier.padding(paddingValues)) {
+            TopHeadLineData(uiState, onNewsClick)
+        }
+    }
+}
+
+
+@Composable
+fun TopHeadLineData(uiState: UiState<List<ArticleEntity>>, onNewsClick: (url: String) -> Unit) {
+    when (uiState) {
+        is UiState.Success -> {
+            NewsArticleList(article = uiState.data, onNewsClick)
+        }
+
+        is UiState.Loading -> {
+            ShowLoading()
+        }
+
+        is UiState.Error -> {
+            ShowError(uiState.message)
+        }
+    }
+}
+
+@Composable
+fun NewsArticleList(article: List<ArticleEntity>, onNewsClick: (url: String) -> Unit) {
+    LazyColumn {
+        items(article, key = { article -> article.url }) { article ->
+            Article(article, onNewsClick)
+        }
+    }
+}
+
+
+@Composable
+fun Article(article: ArticleEntity, onNewsClick: (url: String) -> Unit) {
+    NewsArticleCard(article, onNewsClick)
+}
---
+++ app/src/main/java/me/vikas/newsapp/ui/screens/offline_topheadline/OfflineTopHeadlineViewModel.kt
@@ -0,0 +1,100 @@
+package me.vikas.newsapp.ui.screens.offline_topheadline
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
+import me.vikas.newsapp.data.local.entity.topheadlines.ArticleEntity
+import me.vikas.newsapp.data.repository.OfflineTopHeadlineRepository
+import me.vikas.newsapp.ui.base.UiState
+import me.vikas.newsapp.utils.AppConstant.COUNTRY
+import me.vikas.newsapp.utils.DispatcherProvider
+import me.vikas.newsapp.utils.NetworkHelper
+import javax.inject.Inject
+
+@HiltViewModel
+class OfflineTopHeadlineViewModel @Inject constructor(
+    private val offlineTopHeadlineRepository: OfflineTopHeadlineRepository,
+    private val dispatcherProvider: DispatcherProvider,
+    networkHelper: NetworkHelper
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow<UiState<List<ArticleEntity>>>(UiState.Loading)
+
+    val uiState: StateFlow<UiState<List<ArticleEntity>>> = _uiState
+
+
+    init {
+        if (networkHelper.isInternetConnected()) {
+            fetchArticlesConditionally()
+        } else {
+            fetchArticlesDirectlyFromDB()
+        }
+    }
+
+//    init {
+//        viewModelScope.launch {
+//            if (networkHelper.isInternetConnected() && shouldFetch()) {
+//                fetchArticles()
+//            } else {
+//                fetchArticlesDirectlyFromDB()
+//            }
+//        }
+//    }
+
+    private fun fetchArticlesConditionally() {
+        viewModelScope.launch {
+            if (shouldFetch()) {
+                collectArticlesFlow(offlineTopHeadlineRepository.getArticles(COUNTRY))
+            } else {
+                collectArticlesFlow(offlineTopHeadlineRepository.getArticlesDirectlyFromDB())
+            }
+        }
+    }
+
+
+    private fun collectArticlesFlow(flow: Flow<List<ArticleEntity>>) {
+        viewModelScope.launch {
+            flow.flowOn(dispatcherProvider.io).catch { e ->
+                    _uiState.value = UiState.Error(e.toString())
+                }.collectLatest {
+                    _uiState.value = UiState.Success(it)
+                }
+        }
+    }
+
+
+//    private fun fetchArticles() {
+//        viewModelScope.launch {
+//            offlineTopHeadlineRepository.getArticles(COUNTRY)
+//                .flowOn(dispatcherProvider.io)
+//                .catch { e ->
+//                    _uiState.value = UiState.Error(e.toString())
+//                }.collectLatest {
+//                    _uiState.value = UiState.Success(it)
+//                }
+//        }
+//    }
+
+    private fun fetchArticlesDirectlyFromDB() {
+        viewModelScope.launch {
+            offlineTopHeadlineRepository.getArticlesDirectlyFromDB().flowOn(dispatcherProvider.io)
+                .catch { e ->
+                    _uiState.value = UiState.Error(e.toString())
+                }.collectLatest {
+                    _uiState.value = UiState.Success(it)
+                }
+        }
+    }
+
+    private suspend fun shouldFetch(): Boolean {
+        return offlineTopHeadlineRepository.shouldFetch()
+    }
+
+}
